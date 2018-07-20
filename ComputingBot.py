08dd805480cf3f9b12204658149338f3ad344fb2
TheTripleJHD <41396534+TheTripleJHD@users.noreply.github.com>
2018-07-20 19:32:39 +0800

    Update ComputingBot.py

--- ComputingBot.py
+++ ComputingBot.py
@@ -17,7 +17,8 @@ async def on_ready():
    
 @bot.listen()
 async def on_message(message):
-     text = message.content.split().upper()
+     text = message.content.upper()
+     text.split()
      if re.search(r'\b(da)?vid\b', text, re.I):
         message.add_reaction('\N{SMILING FACE WITH HEART-SHAPED EYES}') 
         
