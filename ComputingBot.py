6c4be6255b4dee3a471f70f3f31a1b30e593a8ea
TheTripleJHD <41396534+TheTripleJHD@users.noreply.github.com>
2018-07-21 02:11:36 +0800

    Update ComputingBot.py

--- ComputingBot.py
+++ ComputingBot.py
@@ -78,16 +78,18 @@ async def on_member_join(member):
 @bot.command()
 async def gay(ctx):
     num = round(random.uniform(0.00,100.00), 2)
-    if num >= 20.00:
-        end = "% Gay."
-        start = " You are "
-        text = start + str(num) + end
-        await ctx.send(ctx.author.mention + text)
-    elif num >= 80.00:
+    if num >= 80.00:
         start = " You are "
         end = "just gay kys."
         text = start + end
         await ctx.send(ctx.author.mention + text)
+      
+    elif num >= 20.00 and num < 80.00:
+        end = "% Gay."
+        start = " You are "
+        text = start + str(num) + end
+        await ctx.send(ctx.author.mention + text)
+        
     else:   
         start = " Lucky! "
         end = "You are not gay."
