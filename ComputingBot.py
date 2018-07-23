20362081fb0e672fe3a17c8cb5835da0d299ccc7
TheTripleJHD <41396534+TheTripleJHD@users.noreply.github.com>
2018-07-24 01:09:15 +0800

    Update ComputingBot.py

--- ComputingBot.py
+++ ComputingBot.py
@@ -78,8 +78,7 @@ async def on_member_join(member):
 @bot.command()
 async def gay(ctx):
     num = round(random.uniform(0.00,100.00), 2)
-    ctx.author = name
-    if name=='sid':
+    if ctx.author=='ɥʇɹɐpᴉs#4852':
          await ctx.send(ctx.author.mention + " Sid is that even a question?")  
     elif num >= 95.00:
         start = " You are just gay kys."
