ad982cfd8cbbc9b2e2ca75b4ad379864c971be92
TheTripleJHD <41396534+TheTripleJHD@users.noreply.github.com>
2018-07-24 01:52:24 +0800

    Update ComputingBot.py

--- ComputingBot.py
+++ ComputingBot.py
@@ -108,15 +108,15 @@ async def gay(ctx):
 async def ok(message):
     return message.author != bot.user
 
-history = await ctx.channel.history(limit=2, check=not_me, reverse=True).flatten()
+    history = await ctx.channel.history(limit=2, check=not_me, reverse=True).flatten()
 
-if not history:
-    # There is no history
-    return
+   if not history:
+       # There is no history
+       return
 
-message = history[0]
+    message = history[0]
 
-await message.add_reaction('\N{OK HAND SIGN}')
+    await message.add_reaction('\N{OK HAND SIGN}')
           
 @bot.command()
 async def rainbow(ctx):
