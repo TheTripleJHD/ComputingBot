5d71d18329ce41eeb3b824ee8a13853c6cc501d1
TheTripleJHD <41396534+TheTripleJHD@users.noreply.github.com>
2018-07-24 02:06:04 +0800

    Update ComputingBot.py

--- ComputingBot.py
+++ ComputingBot.py
@@ -105,18 +105,19 @@ async def gay(ctx):
         await ctx.send(ctx.author.mention + text)
         
 @bot.command()
-async def ok(message):
-    return message.author != bot.user
+async def ok(ctx):
+    def not_me(message):
+        return message.author != bot.user
 
-    history = await ctx.channel.history(limit=2, check=not_me, reverse=True).flatten()
+        history = await ctx.channel.history(limit=2, check=not_me, reverse=True).flatten()
 
-    if not history:
-       # There is no history
-       return
+        if not history:
+           # There is no history
+           return
 
-    message = history[0]
+        message = history[0]
 
-    await message.add_reaction('\N{OK HAND SIGN}')
+        await message.add_reaction('\N{OK HAND SIGN}')
     
 @bot.command()
 async def yes(message):
