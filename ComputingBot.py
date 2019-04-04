18bb4c9206a6b387d5d543118055899fe8ef964b
TheTripleJHD <41396534+TheTripleJHD@users.noreply.github.com>
2019-04-04 18:31:30 +0800

    Update ComputingBot.py

--- ComputingBot.py
+++ ComputingBot.py
@@ -175,11 +175,13 @@ async def rainbow(ctx):
     await ctx.message.add_reaction(emoji = "🖤")
     
 @bot.command()
-async def summon(ctx):  
-    num = choices(population=[1, 2, 3], weights=[0.005, 0.08, 0.915], k=1)
-    await ctx.send(f'{ctx.author.mention}  {num[0]}')
-    
-        
-
-    
+async def summon(ctx):
+    ctx.send(":MysticalScroll: Summoning... :MysticalScroll:")
+    star = choices(population=[5, 4, 3], weights=[0.005, 0.08, 0.915], k=1)
+    fire = choices(population=['Valkyrja', 'Dragon', 'Pheonix', 'Chimera', 'Oracle', 'Occult Girl', 'Dragon Knight',
+                                       'Monkey King', 'Archangel', 'Beast Monk' , 'Hell Lady', 'Pioneer', 'Polar Queen',
+                                       'Ifruit', 'Sea Emperor', 'Dessert Queen', 'Fair King', 'Panda Warrior', 'Unicorn',
+                                       'Paladin', 'Druid', 'Lightning Emperor'], k=1]
+    await ctx.send(ctx.author.mention + " " + fire[0])
+                   
 bot.run(os.getenv('TOKEN'))
