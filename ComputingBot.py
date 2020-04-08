15381b557ff118daf870e64d311e07f6ad4a85f1
TheTripleJHD <41396534+TheTripleJHD@users.noreply.github.com>
2020-04-08 20:35:42 +0800

    Update ComputingBot.py

--- ComputingBot.py
+++ ComputingBot.py
@@ -100,7 +100,7 @@ async def champ(ctx):
                                  'JarvanIV', 'Jax', 'Jayce', 'Jihn', 'Jinx', 'Kaisa', 'Kalista', 'Karthus', 'Kassadin',  'Katarina', 'Kayle', 'Kayn', 'Kennen', 'Khazix',
                                  'Kindred', 'Kled', 'Kogmaw', 'Leblanc', 'LeeSin', 'Leona', 'Lissandra', 'Lucian', 'Lulu', 'Lux', 'Malphite', 'Malzahar', 'Maokai',
                                  'MasterYi', 'MissFortune', 'Mordekaiser', 'Morgana', 'Nami', 'Nasus', 'Nautilus', 'Neeko', 'Nidalee',  'Nocturne', 'Nunu', 'Olaf', 'Orianna',
-                                 'Ornn', 'Pantheon', 'Poppy', 'Pyke', 'Qiyana', 'Quinn', 'Rakan', 'Rammus', 'Reksai', 'Renekton', 'Rengar', 'Riven', 'Rumble,
+                                 'Ornn', 'Pantheon', 'Poppy', 'Pyke', 'Qiyana', 'Quinn', 'Rakan', 'Rammus', 'Reksai', 'Renekton', 'Rengar', 'Riven', 'Rumble',
                                  'Ryze', 'Sejuani', 'Senna', 'Sett', 'Shaco', 'Shen', 'Shyvana', 'Singed', 'Sion', 'Sivir', 'Skarner', 'Sona',  'Soraka', 'Swain',
                                  'Sylas', 'Syndra', 'TahmKench', 'Taliyah', 'Talon', 'Taric', 'Teemo', 'Thresh', 'Tristana', 'Trundle', 'Tryndamere', 'TwistedFate',
                                  'Twitch', 'Udyr', 'Urgot', 'Varus', 'Vayne', 'Veigar', 'Velkoz', 'Vi', 'Viktor', 'Vladimir', 'Volibear', 'Warwick', 'Wukong',
