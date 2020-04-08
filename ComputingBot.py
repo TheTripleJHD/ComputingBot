f7383e63ca21edf543f516280257d1326879beda
TheTripleJHD <41396534+TheTripleJHD@users.noreply.github.com>
2020-04-08 16:25:24 +0800

    Update ComputingBot.py

--- ComputingBot.py
+++ ComputingBot.py
@@ -68,6 +68,7 @@ async def what(ctx):
 @bot.command()
 async def helpme(ctx):
     await ctx.author.send("```List of helpful commands:\n~ping - What is your ping?\n~summon - Summon a monster\n~cute - Who is the cutest?\n~what - What is this discord all about?\n~say - Want me to say something?\n~gay - Check how gay you are!\n~rainbow - Rainboww!!\n~zinnia - Find out who she has traumatised\n~doggo - Awww how cute are dogs?\n~ducc - Waddle waddle..\n~dicc - Big Dicc Energy checker```")
+
 @bot.command()
 async def doggo(ctx):
     url = "https://random.dog/"
