eca4f6bd309e627b7f427e22e9f4184f06983378
TheTripleJHD <41396534+TheTripleJHD@users.noreply.github.com>
2020-04-08 15:24:49 +0800

    Update ComputingBot.py

--- ComputingBot.py
+++ ComputingBot.py
@@ -40,7 +40,7 @@ async def on_message(message):
         text = message.content.upper()
         text.split()
         if re.search(r'\b(ahbar)\b', text, re.I):
-            await message.add_reaction('\N{:poop:}')
+            await message.add_reaction('\N{MIDDLE FINGER}')
 
 @bot.listen()
 async def on_message(message):
