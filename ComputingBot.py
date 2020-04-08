dd2fa3a19bcae5510808f9ce6f225b6245e282e8
TheTripleJHD <41396534+TheTripleJHD@users.noreply.github.com>
2020-04-08 15:19:40 +0800

    Update ComputingBot.py

--- ComputingBot.py
+++ ComputingBot.py
@@ -40,7 +40,7 @@ async def on_message(message):
         text = message.content.upper()
         text.split()
         if re.search(r'\b(ahbar)\b', text, re.I):
-            await message.add_reaction('\N{POOP}')
+            await message.add_reaction('\N{SHIT}')
 
 @bot.listen()
 async def on_message(message):
