8fabfddd071ee6867aa2e231f663c1742c5aaad5
TheTripleJHD <41396534+TheTripleJHD@users.noreply.github.com>
2020-04-08 15:28:03 +0800

    Update ComputingBot.py

--- ComputingBot.py
+++ ComputingBot.py
@@ -40,7 +40,7 @@ async def on_message(message):
         text = message.content.upper()
         text.split()
         if re.search(r'\b(ahbar)\b', text, re.I):
-            await message.add_reaction('\N{MIDDLE FINGER}')
+            await message.add_reaction('\N{ANGRY FACE}')
 
 @bot.listen()
 async def on_message(message):
