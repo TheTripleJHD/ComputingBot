6c64acf79b596085dced537035b34935f19f083a
TheTripleJHD <41396534+TheTripleJHD@users.noreply.github.com>
2020-04-08 16:47:24 +0800

    Update ComputingBot.py

--- ComputingBot.py
+++ ComputingBot.py
@@ -78,7 +78,7 @@ async def doggo(ctx):
     with io.BytesIO() as bp:
         image.save(bp)
         bp.seek(0)
-    await ctx.send(file=discord.File(bp, "dog.jpg"))
+    await ctx.send(file=discord.File(bp, "file.jpg"))
     
 @bot.command()
 async def ducc(ctx):
