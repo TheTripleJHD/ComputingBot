11c6cd793f94b7b4010b4ad1c90e29bef6aafde4
TheTripleJHD <41396534+TheTripleJHD@users.noreply.github.com>
2020-04-09 14:09:14 +0800

    Update ComputingBot.py

--- ComputingBot.py
+++ ComputingBot.py
@@ -16,7 +16,7 @@ bot.remove_command('help')
 async def on_ready():
     print("Bot is ready!")
     await bot.change_presence(
-    activity=discord.Activity(name='Summoners-War',type=discord.ActivityType.playing),status=discord.Status.online)
+    activity=discord.Activity(name='Pokimane',type=discord.ActivityType.watching),status=discord.Status.dnd)
    
 @bot.listen()
 async def on_message(message):
